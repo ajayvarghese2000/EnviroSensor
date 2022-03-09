4d2d493f61376f7f69adb1a590336f41219b0a27
Adi-Kashyap <48380149+Adi-Kashyap@users.noreply.github.com>
2022-03-09 14:20:56 +0000

    Added comments

--- UART_read.py
+++ UART_read.py
@@ -2,6 +2,8 @@ from machine import UART, Pin
 from time import sleep
 #uarttx = Pin(0,Pin.ALT)
 #uartrx = Pin(1,Pin.ALT)
+
+# Initialisation of pins
 enable = Pin(2, Pin.OUT)
 reset = Pin(3, Pin.OUT)
 buff = ["a"]*300
@@ -9,16 +11,21 @@ uart = UART(0,9600)
 i = 0
 enable.high()
 reset.high()
-print(buff)
 
+# Start of loops
 while i<10:
+    # Data Collection
     Buff = str(uart.readline())
     print(i, Buff)
     Dat = ['']*30
+    
+    # Data Processing
     if Buff[2] == str('B') and Buff[3] == str('M'):
         print("SUCCESS")
         indexj = 0
         indexk = 0
+        
+        # Loop data and filter into list
         for char in Buff:
             if char == str('x'):
                 try:
@@ -28,6 +35,7 @@ while i<10:
                 indexj = indexj + 1
             indexk = indexk + 1
         print(Dat)
+        
     else:
         print("FAIL", Buff[2], Buff[3])
     
