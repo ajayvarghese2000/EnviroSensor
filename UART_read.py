74dfff25501173e8923c15712c5b08f0e3362ce6
Dev <adi1.kashyap@gmail.com>
2022-03-10 18:07:06 +0000

    Progress on data processing, Bug outputting

--- UART_read.py
+++ UART_read.py
@@ -1,5 +1,9 @@
+from ubinascii import unhexlify
 from machine import UART, Pin
 from time import sleep
+from math import floor
+from array import array
+from binascii import hexlify
 #uarttx = Pin(0,Pin.ALT)
 #uartrx = Pin(1,Pin.ALT)
 
@@ -12,33 +16,55 @@ i = 0
 enable.high()
 reset.high()
 
-# Start of loops
-while i<10:
-    # Data Collection
-    Buff = str(uart.readline())
-    print(i, Buff)
-    Dat = ['']*30
-    
-    # Data Processing
-    if Buff[2] == str('B') and Buff[3] == str('M'):
-        print("SUCCESS")
-        indexj = 0
-        indexk = 0
+
+def data_process(a):
+
+    # Is Valid Data
+    temp = ['']*30
+    Data = [0]*13
+
+    if a[2] == str('B') and a[3] == str('M') and len(a) < 150:
+        IndexDat = 0
+        IndexBuff = 0
         
         # Loop data and filter into list
-        for char in Buff:
-            if char == str('x'):
-                try:
-                    Dat[indexj] = Buff[indexk+1] + Buff[indexk+2]
-                except IndexError:
+        for char in a:
+            if char == str('x') and IndexDat < len(temp):
+                try:          
+                    temp[IndexDat] = a[IndexBuff+1] + a[IndexBuff+2]
+                except IndexError:  # Error Handling which should not happen
                     print("IndexError")
-                indexj = indexj + 1
-            indexk = indexk + 1
-        print(Dat)
+
+                IndexDat = IndexDat + 1
+
+            IndexBuff = IndexBuff + 1
+
+    print(temp)
+    var = "G"
+
+    # Split output into data array
+    for i in range(len(temp) - 4):
         
-    else:
-        print("FAIL", Buff[2], Buff[3])
-    
-    
-    sleep(1)
+        if i%2 == 0:
+            var = temp[i+2]
+        else:
+            #var = "0x" + var + temp[i+2]
+            Data[int(floor(i/2))] = int("0x15", base=16)
+
+    return Data
+
+        
+
+def Data_Aloc():
+    Data = [int]*13
+
+
+# Start of loops
+while i<2:
+
+    # Data Collection
+    Buff = str(uart.readline())
     i = i+1
+    sleep(1)
+    popom = int("0x15", base=16)
+    print(popom)
